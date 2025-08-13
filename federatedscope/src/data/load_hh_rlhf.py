a2fd7ba5982294401d1856bb814987e227cbb1b0
Jabin Koo <wkqls0829@gmail.com>
2025-08-13 15:01:05 +0900

    bug fix

--- federatedscope/src/data/load_hh_rlhf.py
+++ federatedscope/src/data/load_hh_rlhf.py
@@ -1,7 +1,7 @@
 import logging
 import datasets
 import os
-from federatedscope.core.data import StandaloneDataDict
+from federatedscope.core.data import StandaloneDataDict, ClientData
 from federatedscope.register import register_data
 
 logger = logging.getLogger(__name__)
@@ -82,8 +82,12 @@ def load_hh_rlhf_data(config, client_cfgs=None):
             client_id = i + 1
             train_shard = harmless_train_data.shard(num_shards=harmless_clients_num, index=i)
             test_shard = harmless_test_data.shard(num_shards=harmless_clients_num, index=i)
-            data_dict[client_id] = {'train': train_shard, 'test': test_shard, 'val': test_shard}
-
+            data_dict[client_id] = ClientData(client_cfg=None,
+                                              train_data=train_shard, 
+                                              val_data=test_shard,
+                                              test_data=test_shard)
+            logger.info(f"looking at the fugin td")
+            logger.info(len(data_dict[client_id].train_data))
     helpful_clients_num = client_num - harmless_clients_num
     logger.info(f"Assigning helpfulness data to {helpful_clients_num} clients...")
     if helpful_clients_num > 0:
@@ -91,7 +95,11 @@ def load_hh_rlhf_data(config, client_cfgs=None):
             client_id = i + harmless_clients_num + 1
             train_shard = helpful_train_data.shard(num_shards=helpful_clients_num, index=i)
             test_shard = helpful_test_data.shard(num_shards=helpful_clients_num, index=i)
-            data_dict[client_id] = {'train': train_shard, 'test': test_shard, 'val': test_shard}
+            data_dict[client_id] = ClientData(client_cfg=None,
+                                              train_data=train_shard, 
+                                              val_data=test_shard,
+                                              test_data=test_shard)
+    logger.info(len(data_dict[1].train_data))
 
     logger.info("Finished creating heterogeneous data distribution.")
     return StandaloneDataDict(data_dict, config), config
