4ee327aab89658968df05a7882d9b0a3bcecf402
Jabin Koo <jbkoo@postech.ac.kr>
2026-01-20 14:59:01 +0900

    Fix IndentationError: Rewrite if statement on line 558 to use parentheses for line continuation

--- federatedscope/core/workers/client.py
+++ federatedscope/core/workers/client.py
@@ -555,7 +555,8 @@ class Client(BaseClient):
             logger.info(formatted_eval_res)
 
             # Only update best result if metrics are not empty
-            if formatted_eval_res and 'Results_raw' in formatted_eval_res and formatted_eval_res['Results_raw']:
+            if (formatted_eval_res and 'Results_raw' in formatted_eval_res and 
+                formatted_eval_res['Results_raw']):
                 update_best_this_round = self._monitor.update_best_result(
                     self.best_results,
                     formatted_eval_res['Results_raw'],
